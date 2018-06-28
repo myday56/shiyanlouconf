a20baed200e70643a39ce5bfaa58889333cb9846
myday56 <leletou56@live.com>
2018-06-29 00:05:53 +0800

    le

--- tel_copy_0628.py
+++ tel_copy_0628.py
@@ -3,4 +3,13 @@ from ftplib import FTP
 ftp = FTP()
 ftp.connect('127.0.0.1')
 ftp.login(user='',passwd='')
-print(ftp.getwelcome())
+# print(ftp.getwelcome())
+# ftp.dir()
+date = '20150629'
+tel_num = '8663187'
+filename = '151449-O-0006-8663184-S.wav'
+file_handle = open(filename, "wb").write
+bufsize = 1024
+ftp.cwd(date+'/'+tel_num)
+# ftp.dir()
+ftp.retrbinary("RETR "+filename, file_handle, bufsize)
