7fa99ef736f79328afe1eaf7a3aab9f8cdfb21b4
myday56 <leletou56@live.com>
2018-06-29 00:29:32 +0800

    le

--- tel_copy_0628.py
+++ tel_copy_0628.py
@@ -8,9 +8,18 @@ ftp.login(user='', passwd='')
 date = '20150629'
 tel_num = '8663187'
 filename = '151449-O-0006-8663184-S.wav'
-file_handle = open(filename, "wb").write
+
 bufsize = 1024
 ftp.cwd(date+'/'+tel_num)
 # ftp.dir()
-ftp.retrbinary("RETR "+ filename, file_handle, bufsize)
-ftp.pwd()
+
+print(ftp.pwd())
+
+
+def down_rec():
+    file_handle = open(filename, "wb").write
+    bufsize = 1024
+    ftp.retrbinary("RETR " + filename, file_handle, bufsize)
+
+
+down_rec()
