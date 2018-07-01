a37b47f0974269f5c606d4cab526d8b3016f397c
myday56 <leletou56@live.com>
2018-07-01 21:26:27 +0800

    0701

--- tel_copy_0628.py
+++ tel_copy_0628.py
@@ -6,20 +6,23 @@ ftp.login(user='', passwd='')
 # print(ftp.getwelcome())
 # ftp.dir()
 date = '20150629'
-tel_num = '8663187'
-filename = '151449-O-0006-8663184-S.wav'
+# tel_num = '8663187' #电话号
+# filename = '151449-O-0006-8663184-S.wav' #文件名
 
 bufsize = 1024
-ftp.cwd(date+'/'+tel_num)
+# ftp.cwd(date+'/'+tel_num)  #切换目录
 # ftp.dir()
+a = []
+a.append(ftp.retrlines('nlst'))
 
-print(ftp.pwd())
+# print(ftp.pwd())
 
 
 def down_rec():
     file_handle = open(filename, "wb").write
     bufsize = 1024
     ftp.retrbinary("RETR " + filename, file_handle, bufsize)
+    print('已下载文件', filename)
 
 
-down_rec()
+# down_rec() #下载
