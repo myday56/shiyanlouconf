e73cf8a2bd13f59a50b385f048c5fee83833fe09
myday56 <leletou56@live.com>
2018-07-11 17:15:24 +0800

    Update tel_copy_0628.py

--- tel_copy_0628.py
+++ tel_copy_0628.py
@@ -1,31 +1,49 @@
 from ftplib import FTP
 import os
-ftp = FTP()
-ftp.connect('127.0.0.1')
-ftp.login(user='', passwd='')
+
 # print(ftp.getwelcome())
 # ftp.dir()
+url = '192.168.96.55'
 date = '20150629'
-tel_num = '8663187' #电话号
-filename = '151449-O-0006-8663184-S.wav' #文件名
+tel_num = '8663187'
+# filename = '151449-O-0006-8663184-S.wav'
+lujing = 'C:\\Users\\kfb\\Desktop\\py\\code\\tel'
 
-bufsize = 1024
-# ftp.cwd(date+'/'+tel_num)  #切换目录
-a = []
-a = ftp.retrlines('list')
-# print(a.split())
-for i in a[1:3]:
-    print(i)
-# a.append(ftp.retrlines('nlst'))
+
+
+# ftp.cwd(date+'/'+tel_num)
+
 
 # print(ftp.pwd())
+ftp = FTP(url)
+# ftp.connect(url)
+ftp.login(user='', passwd='')
+tel_date_list = []
+tel_date_temp = []
+tel_date = []
+ftp.dir("",tel_date_list.append)
+for x in tel_date_list:
+    tel_date_temp.append(x[-8:])
+    tel_date = tel_date_temp[2:]
+    # ftp.cwd(str(tel_date))
+    # break
+print(tel_date[0])
+ftp.cwd(tel_date[0]+'/'+tel_num)
+filename_list = []
+filename_temp = []
+filename = []
+ftp.dir("",filename_list.append)
+for x in filename_list:
+    filename_temp.append(x[55:])
+    filename = filename_temp[2:]
+# print(a.split())
+print(filename[0])
+# def down_rec(): #下载
+#     file_handle = open(filename, "wb").write
+#     bufsize = 1024
+#     ftp.retrbinary("RETR " + filename, file_handle, bufsize)
 
 
-def down_rec():
-    file_handle = open(filename, "wb").write
-    bufsize = 1024
-    ftp.retrbinary("RETR " + filename, file_handle, bufsize)
-    print('已下载文件', filename)
-
-
-# down_rec() #下载
+# down_rec()
+# ftp_connect()
+# create_dic()  #def create_dic() 定义函数
